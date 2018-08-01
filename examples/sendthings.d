219cd229a6ad97c1547f4c9ac38559fa25ef4721
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-01 19:27:37 +0200

    Add an example, showing different send* methods

---
+++ examples/sendthings.d
@@ -0,0 +1,122 @@
+#!/usr/bin/env dub
+/+
+dub.json:
+{
+	"name": "sendthings",
+	"descripton": "Telegram bot with multiple commands for sending random photos, audios, etc",
+	"license": "Public Domain",
+	"dependencies": {
+		"tg-d": {"path": "../"},
+		"vibe-d:http": "~>0.8",
+		"vibe-d:tls": "~>0.8",
+	},
+	"subConfigurations": {
+		"vibe-d:tls": "openssl-1.1"
+	}
+}
++/
+
+/**
+ * This example is powered by:
+ * - https://loremflickr.com
+ * - https://freemusicarchive.org
+ * - https://archive.org
+ * - https://commons.wikimedia.org
+ * All rights for videos, photos and animations used in this example go to their respective owners.
+ */
+
+import core.time        : seconds;
+import std.algorithm    : startsWith;
+import std.format       : format;
+import std.random       : uniform, choice;
+import tg.d;
+import vibe.core.args   : readRequiredOption;
+import vibe.core.core   : runApplication, setTimer;
+import vibe.core.log    : logInfo;
+import vibe.http.client : requestHTTP;
+
+int main() {
+	auto Bot = TelegramBot(
+		"token|t".readRequiredOption!string("Bot token to use. Ask BotFather for it")
+	);
+
+	auto me = Bot.getMe;
+	"This bot info:"     .logInfo;
+	"\tID: %d"           .logInfo(me.id);
+	"\tIs bot: %s"       .logInfo(me.is_bot);
+	"\tFirst name: %s"   .logInfo(me.first_name);
+	"\tLast name: %s"    .logInfo(me.last_name.isNull     ? "null" : me.last_name);
+	"\tUsername: %s"     .logInfo(me.username.isNull      ? "null" : me.username);
+	"\tLanguage code: %s".logInfo(me.language_code.isNull ? "null" : me.language_code);
+
+	"Setting up the timer".logInfo;
+	1.seconds.setTimer(
+		() {
+			foreach(update; Bot.updateGetter) {
+				if(update.message.isNull || update.message.text.isNull) // We are only caring about text messages
+					continue;
+
+				if(update.message.text.startsWith("/photo")) {
+					"Got /photo command, sending random photo".logInfo;
+					Bot.sendPhoto(
+						update.message.chat.id,
+						"https://loremflickr.com/320/240/dog,puppy?random=%d".format(uniform(0, int.max))
+					);
+				} else if(update.message.text.startsWith("/audio")) {
+					"Got /audio command, sending random audio".logInfo;
+
+					SendAudioMethod m = {
+						chat_id: update.message.chat.id,
+					};
+
+					"https://freemusicarchive.org/featured.json".requestHTTP(
+						(scope req) {},
+						(scope res) {
+							auto j = res.readJson["aTracks"];
+							auto track = j[].choice;
+		
+							m.audio      = track["track_file_url"].get!string;
+							m.performer  = track["artist_name"].get!string;
+							m.title      = track["track_title"].get!string;
+							m.thumb      = track["track_image_file"].get!string;
+							m.caption    = "Licensed under the terms of [%s](%s) license".format(
+								track["license_title"].get!string,
+								track["license_url"].get!string,
+							);
+							m.parse_mode = ParseMode.markdown;
+						}
+					);
+
+					Bot.sendAudio(m);
+				} else if(update.message.text.startsWith("/document")) {
+					"Got /document command, sending D specification".logInfo;
+
+					Bot.sendDocument(update.message.chat.id, "https://dlang.org/dlangspec.pdf");
+				} else if(update.message.text.startsWith("/video")) {
+					"Got /video command, sending the train".logInfo;
+
+					Bot.sendVideo(
+						update.message.chat.id, 
+						"https://archive.org/download/youtube--e1u7Fgoocc/L_Arrivee_D_un_Train_En_Gare_De_La_Ciotat_1895--e1u7Fgoocc.mp4"
+					);
+				} else if(update.message.text.startsWith("/animation")) {
+					"Got /animation command, sending an animation".logInfo;
+
+					SendAnimationMethod m = {
+						chat_id: update.message.chat.id,
+						animation: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif",
+						caption: "By Marvel (Based upon a NASA image, see [1].) [GFDL](http://www.gnu.org/copyleft/fdl.html) or [CC-BY-SA-3.0](http://creativecommons.org/licenses/by-sa/3.0/), via Wikimedia Commons",
+						parse_mode: ParseMode.markdown,
+					};
+					Bot.sendAnimation(m);
+				} else {
+					Bot.sendMessage(update.message.chat.id, "What do you mean by that?");
+				}
+			}
+		},
+		true); // To run this timer not just once, but every second
+
+	"Running the bot".logInfo;
+	return runApplication();
+}
+
